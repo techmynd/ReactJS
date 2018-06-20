46c0f1e4cbab250a316198a5c94e4db968890d1c
techmynd <admin@techmynd.com>
2018-06-20 21:19:32 +0500

    react two

---
+++ react-two/src/components/About/About.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './About.css';
+
+class About extends Component {
+	render() {
+		return (
+			<div>
+				<h2>About</h2>
+				<p>Cras justo odio, dapibus ac facilisis, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+				<p>Cras justo odio, dapibus ac facilisis, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+				<p>Cras justo odio, dapibus ac facilisis, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+				<p>Cras justo odio, dapibus ac facilisis, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+				<p>Cras justo odio, dapibus ac facilisis, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+			</div>
+		);
+	};
+};
+export default About;
