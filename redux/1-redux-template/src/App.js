e7309f12f09fc849f3f1a903ea1355e51ac380cb
javed khalil <admin@techmynd.com>
2020-02-16 22:36:01 +0500

    update

--- redux/1-redux-template/src/App.js
+++ redux/1-redux-template/src/App.js
@@ -5,13 +5,13 @@ import { connect } from "react-redux";
 import * as actionTypes from "./store/actions/types";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    // this.state = {
-    //   toggle: true,
-    //   counter: 0,
-    // };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     toggle: true,
+  //     counter: 0,
+  //   };
+  // }
 
   // handleClick = () => {
   //   this.setState({
